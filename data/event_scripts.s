aed9bf89b34f89e745eea17aea8b9635fb336697
Cippy99 <antocipani@gmail.com>
2023-12-30 10:45:35 +0100

    Mauville CIty Gym scripts

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -693,6 +693,7 @@ EventScript_BackupMrBrineyLocation::
 	.include "data/scripts/set_gym_trainers.inc"
 	.include "data/scripts/important_fights/gym1.inc"
 	.include "data/scripts/important_fights/gym2.inc"
+	.include "data/scripts/important_fights/gym3.inc"
 	.include "data/scripts/important_fights/texts.inc"
 
 Common_EventScript_ShowBagIsFull::
